bf88fb6db3c160deabbe3ca9a346a5a7e5e30e86
25yt551 <amirrchr@gmail.com>
2025-06-15 00:15:51 +0200

    Update users.php

--- api/users.php
+++ api/users.php
@@ -76,6 +76,25 @@
               "Client_VisibleSkin8": "0000",
               "Client_VisibleSkin9": "0000",
               "Client_VisibleSkin10": "0000"
+        },
+        {
+              "id": 5,
+              "cliente_NOMBRE": "MALIK",
+              "cliente_ID": "gg_10639471083542334765",
+              "Client_KeyAccecs": "XTPRIVATESKIN",
+              "cliente_DateExpired": "2025-07-30",
+              "status": 1,
+              "Client_VisibleSkin": "0000",
+              "Client_VisibleSkin1": "0000",
+              "Client_VisibleSkin2": "0000",
+              "Client_VisibleSkin3": "0000",
+              "Client_VisibleSkin4": "0000",
+              "Client_VisibleSkin5": "0000",
+              "Client_VisibleSkin6": "0000",
+              "Client_VisibleSkin7": "0000",
+              "Client_VisibleSkin8": "0000",
+              "Client_VisibleSkin9": "0000",
+              "Client_VisibleSkin10": "0000"
         }
     ],
     "timestamp": 1749885996,
