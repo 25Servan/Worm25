4f6994485191f09a7aeae9a9fa0668062c1608c0
25Servan <02amedxg@gmail.com>
2025-06-14 11:02:54 +0300

    Add files via upload

---
+++ api/users.php
@@ -0,0 +1,26 @@
+{
+    "success": true,
+    "Users": [
+        {
+            "id": 1,
+            "cliente_NOMBRE": "25/SERVAN",
+            "cliente_ID": "gg_108422023604413042881",
+            "Client_KeyAccecs": "XTPRIVATESKIN",
+            "cliente_DateExpired": "2025-06-30",
+            "status": 1,
+            "Client_VisibleSkin": "0000",
+            "Client_VisibleSkin1": "0000",
+            "Client_VisibleSkin2": "0000",
+            "Client_VisibleSkin3": "0000",
+            "Client_VisibleSkin4": "0000",
+            "Client_VisibleSkin5": "0000",
+            "Client_VisibleSkin6": "0000",
+            "Client_VisibleSkin7": "0000",
+            "Client_VisibleSkin8": "0000",
+            "Client_VisibleSkin9": "0000",
+            "Client_VisibleSkin10": "0000"
+        }
+    ],
+    "timestamp": 1749885996,
+    "error": null
+}
