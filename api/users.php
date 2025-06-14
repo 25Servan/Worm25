c966bb243aa0cdf62ed1edca2488d027f010b3ec
25Servan <02amedxg@gmail.com>
2025-06-14 11:57:43 +0300

    Update users.php

--- api/users.php
+++ api/users.php
@@ -4,6 +4,25 @@
         {
             "id": 1,
             "cliente_NOMBRE": "25/SERVAN",
+            "cliente_ID": "gg_106263452150533879793",
+            "Client_KeyAccecs": "XTPRIVATESKIN",
+            "cliente_DateExpired": "2046-06-30",
+            "status": 1,
+            "Client_VisibleSkin": "0000",
+            "Client_VisibleSkin1": "0000",
+            "Client_VisibleSkin2": "0000",
+            "Client_VisibleSkin3": "0000",
+            "Client_VisibleSkin4": "0000",
+            "Client_VisibleSkin5": "0000",
+            "Client_VisibleSkin6": "0000",
+            "Client_VisibleSkin7": "0000",
+            "Client_VisibleSkin8": "0000",
+            "Client_VisibleSkin9": "0000",
+            "Client_VisibleSkin10": "0000"
+        }
+        {
+            "id": 2,
+            "cliente_NOMBRE": "25/YT",
             "cliente_ID": "gg_108422023604413042881",
             "Client_KeyAccecs": "XTPRIVATESKIN",
             "cliente_DateExpired": "2025-06-30",
@@ -20,6 +39,8 @@
             "Client_VisibleSkin9": "0000",
             "Client_VisibleSkin10": "0000"
         }
+
+
     ],
     "timestamp": 1749885996,
     "error": null
