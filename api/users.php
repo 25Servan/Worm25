02b43b032e551dacc593d0f27c9fdc3cb850e8e8
25yt551 <amirrchr@gmail.com>
2025-06-14 15:28:42 +0200

    Update users.php

--- api/users.php
+++ api/users.php
@@ -40,26 +40,6 @@
             "Client_VisibleSkin10": "0000"
         },
         {
-            {
-            "id": 3,
-            "cliente_NOMBRE": "Ball/pool YT",
-            "cliente_ID": "gg_100512750331575053600",
-            "Client_KeyAccecs": "XTPRIVATESKIN",
-            "cliente_DateExpired": "2046-06-30",
-            "status": 1,
-            "Client_VisibleSkin": "0000",
-            "Client_VisibleSkin1": "0000",
-            "Client_VisibleSkin2": "0000",
-            "Client_VisibleSkin3": "0000",
-            "Client_VisibleSkin4": "0000",
-            "Client_VisibleSkin5": "0000",
-            "Client_VisibleSkin6": "0000",
-            "Client_VisibleSkin7": "0000",
-            "Client_VisibleSkin8": "0000",
-            "Client_VisibleSkin9": "0000",
-            "Client_VisibleSkin10": "0000"
-        },
-        {
 
 
     ],
