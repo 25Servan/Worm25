094fdaa0045dc1e04d128ba80156b214d71cbead
25yt551 <amirrchr@gmail.com>
2025-06-15 20:47:58 +0200

    Update users.php

--- api/users.php
+++ api/users.php
@@ -190,6 +190,25 @@
               "Client_VisibleSkin8": "0000",
               "Client_VisibleSkin9": "0000",
               "Client_VisibleSkin10": "0000"
+        },
+        {
+              "id": 11,
+              "cliente_NOMBRE": "Yusef salah",
+              "cliente_ID": "gg_103371055113889837997",
+              "Client_KeyAccecs": "XTPRIVATESKIN",
+              "cliente_DateExpired": "2025-07-31",
+              "status": 1,
+              "Client_VisibleSkin": "0000",
+              "Client_VisibleSkin1": "0000",
+              "Client_VisibleSkin2": "0000",
+              "Client_VisibleSkin3": "0000",
+              "Client_VisibleSkin4": "0000",
+              "Client_VisibleSkin5": "0000",
+              "Client_VisibleSkin6": "0000",
+              "Client_VisibleSkin7": "0000",
+              "Client_VisibleSkin8": "0000",
+              "Client_VisibleSkin9": "0000",
+              "Client_VisibleSkin10": "0000"
         }
     ],
     "timestamp": 1749885996,
