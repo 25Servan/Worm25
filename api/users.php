2d5cf67359c4994b822606401191ccaadcc3c5d4
25yt551 <amirrchr@gmail.com>
2025-06-18 13:02:58 +0200

    Update users.php

--- api/users.php
+++ api/users.php
@@ -21,25 +21,6 @@
             "Client_VisibleSkin10": "0000"
         },
         {
-            "id": 2,
-            "cliente_NOMBRE": "NO",
-            "cliente_ID": "gg_NO",
-            "Client_KeyAccecs": "XTPRIVATESKIN",
-            "cliente_DateExpired": "2034-12-05",
-            "status": 1,
-            "Client_VisibleSkin": "0000",
-            "Client_VisibleSkin1": "0000",
-            "Client_VisibleSkin2": "0000",
-            "Client_VisibleSkin3": "0000",
-            "Client_VisibleSkin4": "0000",
-            "Client_VisibleSkin5": "0000",
-            "Client_VisibleSkin6": "0000",
-            "Client_VisibleSkin7": "0000",
-            "Client_VisibleSkin8": "0000",
-            "Client_VisibleSkin9": "0000",
-            "Client_VisibleSkin10": "0000"
-        },
-        {
               "id": 3,
               "cliente_NOMBRE": "25YT",
               "cliente_ID": "gg_116152060195741273040",
