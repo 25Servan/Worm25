3ce6ddd4313295e036873e09da19e7de983952f1
25yt551 <amirrchr@gmail.com>
2025-06-27 15:12:53 +0200

    Update users.php

--- api/users.php
+++ api/users.php
@@ -247,6 +247,25 @@
               "Client_VisibleSkin8": "0000",
               "Client_VisibleSkin9": "0000",
               "Client_VisibleSkin10": "0000"
+        },
+        {
+              "id": 15,
+              "cliente_NOMBRE": "ali",
+              "cliente_ID": "gg_102165733998306860874",
+              "Client_KeyAccecs": "XTPRIVATESKIN",
+              "cliente_DateExpired": "2025-09-01",
+              "status": 1,
+              "Client_VisibleSkin": "0000",
+              "Client_VisibleSkin1": "0000",
+              "Client_VisibleSkin2": "0000",
+              "Client_VisibleSkin3": "0000",
+              "Client_VisibleSkin4": "0000",
+              "Client_VisibleSkin5": "0000",
+              "Client_VisibleSkin6": "0000",
+              "Client_VisibleSkin7": "0000",
+              "Client_VisibleSkin8": "0000",
+              "Client_VisibleSkin9": "0000",
+              "Client_VisibleSkin10": "0000"
         }
         
         
